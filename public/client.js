dae718a9b0f00701ee94239616eb856a9d746255
hsgwKei <63716217+hsgwKei@users.noreply.github.com>
2021-05-25 21:45:43 +0900

    Add files via upload

--- public/client.js
+++ public/client.js
@@ -18,7 +18,7 @@ const g_elementVideoLocal = document.getElementById( "video_local" );
 //const g_elementVideoRemote = document.getElementById( "video_remote" );
 //const g_elementAudioRemote = document.getElementById( "audio_remote" );
 
-//const g_elementBtnLeave = docement.getElementById("btn_leave");
+const g_elementBtnLeave = docement.getElementById("btn_leave");
 
 //const g_elementTextMessageForSend = document.getElementById( "text_message_for_send" );
 //const g_elementTextareaMessageReceived = document.getElementById( "textarea_message_received" );
